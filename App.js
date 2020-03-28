ff13fd4a5560a43f26f71bb18c2bc490c06a38f8
Nicole Leong <nicole@massiveinfinity.com>
2020-03-28 23:04:39 +0800

    Render weather forecast

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, Image } from 'react-native';
+
+const sunny = require('./assets/sunny.png')
 
 const App = () => {
   return (
@@ -7,6 +9,14 @@ const App = () => {
       <StatusBar barStyle="light-content" />
       <View style={styles.header}>
         <Text style={styles.headerText}>Singapore, Singapore</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>Wednesday, 11 Jan 2020 </Text>
+        <Text style={styles.temperature}>82</Text>
+        <Text style={styles.weather}>Sunny</Text>
+        <View style={styles.weatherIconWrapper}>
+          <Image source={sunny} style={styles.weatherIcon} />
+        </View>
       </View>
     </>
   );
@@ -23,6 +33,43 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  text: {
+    padding: 20,
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  weatherIconWrapper: {
+    top: -85,
+    left: '57%',
+  },
+  temperature: {
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: '500',
+    fontSize: 48,
+  },
+  weatherWrapper: {
+    flex: 1,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  weather: {
+    color: 'grey',
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  weatherIcon: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
   },
 });
 
